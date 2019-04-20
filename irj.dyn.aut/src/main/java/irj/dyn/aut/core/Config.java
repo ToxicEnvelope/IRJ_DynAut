236df9132dc77f4e6f9b31d43d08c54ee3bf9d12
Yahav N. Hoffman <ynhoffman@gmail.com>
2019-04-20 23:28:45 +0300

    append logging capabilities

--- irj.dyn.aut/src/main/java/irj/dyn/aut/core/Config.java
+++ irj.dyn.aut/src/main/java/irj/dyn/aut/core/Config.java
@@ -17,7 +17,7 @@ public class Config {
 
     private static final String TEST_OUTPUT_DIR = "/test-output";
     private static Config instance = null;
-    private static LinkedHashMap config;
+    //private static LinkedHashMap config;
 
     /**
      * Constructor
@@ -25,7 +25,7 @@ public class Config {
      * @throws IOException exception
      */
     protected Config() throws IOException {
-        config = getConfigData();
+        //config = getConfigData();
     }
 
     /**
@@ -64,95 +64,6 @@ public class Config {
     public static String getTestOutputDir() {
         System.out.println("\nCalling "+ new Throwable().getStackTrace()[0].getMethodName());
         return getProjectRootDir().concat(TEST_OUTPUT_DIR);
-    }
-
-    /**
-     * Returns LinkedHashMap yaml-parsed data from config file
-     *
-     * @return LinkedHashMap
-     * @throws NullPointerException exception
-     */
-    public static LinkedHashMap getConfigData() throws NullPointerException
-    {
-        System.out.println("\nCalling "+ new Throwable().getStackTrace()[0].getMethodName());
-        final String defaultFileName = "/config.yaml";
-        final String localFileName = "/local-config.yaml";
-
-        Yaml yaml = new Yaml();
-        LinkedHashMap configData;
-
-        logger.info("Locating local configuration file, \"" + localFileName + "\"...");
-        try {
-
-            InputStream localFile = Config.class.getResourceAsStream(localFileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(localFile));
-
-            logger.info("Local configuration file, \"" + localFileName + "\" found. Parsing yaml file...");
-            configData = (LinkedHashMap) yaml.load(reader);
-
-        } catch (NullPointerException e) {
-
-            logger.info("Could not locate/read local configuration file \"" + localFileName
-                    + "\". Locating default configuration file, \"" + defaultFileName + "\"...");
-
-            try {
-
-                InputStream defaultFile = Config.class.getResourceAsStream(defaultFileName);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(defaultFile));
-
-                logger.info("Default configuration file, \"" + defaultFileName + "\" found. Parsing yaml file...");
-                configData = (LinkedHashMap) yaml.load(reader);
-
-            } catch (NullPointerException ex) {
-
-                logger.info("Could not locate/read default configuration file \"" + defaultFileName + "\"...");
-                throw ex;
-
-            }
-
-        }
-
-        return configData;
-
-    }
-
-    /**
-     * return the current running OS
-     * @return - String
-     * @author Yahav N. Hoffman
-     */
-    public static String getWorkingOS() {
-        System.out.println("\nCalling "+ new Throwable().getStackTrace()[0].getMethodName());
-        return System.getProperty("os.name");
-    }
-
-    public static LinkedHashMap getConfigDataForJar() throws NullPointerException
-    {
-        System.out.println("\nCalling "+ new Throwable().getStackTrace()[0].getMethodName());
-        final String localFileName = "/conf/envProperties.yaml";
-
-        Yaml yaml = new Yaml();
-        LinkedHashMap configData;
-
-        logger.info("Locating local configuration file, \"" + localFileName + "\"...");
-
-        InputStream localFile = Config.class.getResourceAsStream(localFileName);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(localFile));
-
-        logger.info("Local configuration file, \"" + localFileName + "\" found. Parsing yaml file...");
-        configData = (LinkedHashMap) yaml.load(reader);
-        /** TODO - solve NullPointerException**/
-        /*try {
-            InputStream localFile = Config.class.getResourceAsStream(localFileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(localFile));
-            logger.info("Local configuration file, \"" + localFileName + "\" found. Parsing yaml file...");
-            configData = (LinkedHashMap) yaml.load(reader);
-        } catch (NullPointerException e) {
-            logger.info("Could not locate/read local configuration file \"" + localFileName + "\"...");
-            throw e;
-        }*/
-
-        return configData;
     }
 
 }
